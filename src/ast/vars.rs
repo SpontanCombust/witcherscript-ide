30c16bd2270b79db3d2a0dc8ba34405499531d37
SpontanCombust <61706594+SpontanCombust@users.noreply.github.com>
2023-11-01 16:06:00 +0100

    var declaration

--- src/ast/vars.rs
+++ src/ast/vars.rs
@@ -1,5 +1,26 @@
+use super::classes::AccessModifier;
+use bitmask_enum::bitmask;
+
 #[derive(Debug, PartialEq, Eq)]
 pub struct TypeAnnotation {
-    name: String,
-    generic_argument: Option<String> // only used for arrays
+    pub name: String,
+    pub generic_argument: Option<String> // only used for arrays
+}
+
+#[derive(Debug, PartialEq, Eq)]
+pub struct VarDeclaration {
+    pub access_modifier: Option<AccessModifier>,
+    pub specifiers: VarSpecifiers,
+
+    pub name: String,
+    pub var_type: TypeAnnotation
+}
+
+#[bitmask]
+pub enum VarSpecifiers {
+    Const,
+    Editable,
+    Import,
+    Inlined,
+    Saved,
 }
