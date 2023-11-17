19d77479a0b37abcc37241a90fdf48ecf6ec87c7
SpontanCombust <61706594+SpontanCombust@users.noreply.github.com>
2023-11-17 02:01:03 +0100

    fix decoding and parsing from rope

--- core/src/script.rs
+++ core/src/script.rs
@@ -1,10 +1,10 @@
 use std::{
     fs::File,
-    io,
+    io::{self, Write, BufReader, Read, BufRead},
     path::{PathBuf, Path}
 };
 
-use ropey::Rope;
+use ropey::{Rope, RopeBuilder};
 use thiserror::Error;
 use tree_sitter::{Parser, Tree, LanguageError};
 use encoding_rs_io::DecodeReaderBytes;
@@ -33,17 +33,28 @@ impl Script {
 
         let f = File::open(&path).map_err(FileOpenError)?;
         let decoder = DecodeReaderBytes::new(f);
+        let mut reader = BufReader::new(decoder);
 
-        let rope = Rope::from_reader(decoder).map_err(FileReadError)?;
+        let mut builder = RopeBuilder::new();
+        let mut line = String::new();
+        while let Ok(b) = reader.read_line(&mut line) {
+            if b == 0 {
+                break;
+            }
+            builder.append(&line);
+            line.clear();
+        }
+        let rope = builder.finish();
 
         let mut parser = Parser::new();
         parser.set_language(tree_sitter_witcherscript::language()).map_err(ParserInitError)?;
 
-        let parse_tree = parser.parse_with(&mut |offset, _| {
-            if offset < rope.len_bytes() {
-                rope.chunk_at_byte(offset).0
+        let parse_tree = parser.parse_with(&mut |byte, _| {
+            if byte <= rope.len_bytes() {
+                let (chunk, start_byte, _, _) = rope.chunk_at_byte(byte);
+                &chunk.as_bytes()[byte - start_byte..]
             } else {
-                &""
+                &[]
             }
         }, None).unwrap();
 
@@ -52,5 +63,67 @@ impl Script {
             rope,
             parse_tree
         })
+    }
+}
+
+// Acquired from tree-sitter-cli
+// Will delete later
+#[test]
+fn test() {
+    let s1 = Script::from_file("D:\\Git-repo\\tw3-scripts\\core\\math.ws").unwrap();
+    
+    let stdout = io::stdout();
+    let mut stdout = stdout.lock();
+    let mut cursor = s1.parse_tree.walk();
+    let mut needs_newline = false;
+    let mut indent_level = 0;
+    let mut did_visit_children = false;
+    loop {
+        let node = cursor.node();
+        let is_named = node.is_named();
+        if did_visit_children {
+            if is_named {
+                stdout.write(b")").unwrap();
+                needs_newline = true;
+            }
+            if cursor.goto_next_sibling() {
+                did_visit_children = false;
+            } else if cursor.goto_parent() {
+                did_visit_children = true;
+                indent_level -= 1;
+            } else {
+                break;
+            }
+        } else {
+            if is_named {
+                if needs_newline {
+                    stdout.write(b"\n").unwrap();
+                }
+                for _ in 0..indent_level {
+                    stdout.write(b"  ").unwrap();
+                }
+                let start = node.start_position();
+                let end = node.end_position();
+                if let Some(field_name) = cursor.field_name() {
+                    write!(&mut stdout, "{}: ", field_name).unwrap();
+                }
+                write!(
+                    &mut stdout,
+                    "({} [{}, {}] - [{}, {}]",
+                    node.kind(),
+                    start.row + 1,
+                    start.column + 1,
+                    end.row + 1,
+                    end.column + 1
+                ).unwrap();
+                needs_newline = true;
+            }
+            if cursor.goto_first_child() {
+                did_visit_children = false;
+                indent_level += 1;
+            } else {
+                did_visit_children = true;
+            }
+        }
     }
 }
