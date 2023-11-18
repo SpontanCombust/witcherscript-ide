9539acb1501d1591a90679f2baa4d5dbb7e4d22e
SpontanCombust <61706594+SpontanCombust@users.noreply.github.com>
2023-11-18 22:28:01 +0100

    use shrinkwrap

--- core/src/tokens/identifier.rs
+++ core/src/tokens/identifier.rs
@@ -1,33 +1,4 @@
-use std::ops::{DerefMut, Deref};
+use shrinkwraprs::Shrinkwrap;
 
-use crate::{NamedSyntaxNode, SyntaxNode};
-
-
-#[derive(Debug, Clone, PartialEq, Eq, PartialOrd, Ord)]
+#[derive(Shrinkwrap, Debug, Clone, PartialEq, Eq, PartialOrd, Ord)]
 pub struct Identifier(String);
-
-impl Deref for Identifier {
-    type Target = String;
-
-    fn deref(&self) -> &Self::Target {
-        &self.0
-    }
-}
-
-impl DerefMut for Identifier {
-    fn deref_mut(&mut self) -> &mut Self::Target {
-        &mut self.0
-    }
-}
-
-impl From<String> for Identifier {
-    fn from(value: String) -> Self {
-        Self(value)
-    }
-}
-
-impl From<&str> for Identifier {
-    fn from(value: &str) -> Self {
-        Self(value.into())
-    }
-}
