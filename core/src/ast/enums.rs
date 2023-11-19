5e475a76cde3e4e62102110e782947a833168892
SpontanCombust <61706594+SpontanCombust@users.noreply.github.com>
2023-11-19 18:37:14 +0100

    enum and struct nodes

--- core/src/ast/enums.rs
+++ core/src/ast/enums.rs
@@ -1,15 +1,53 @@
-use crate::tokens::{Identifier, Spanned};
+use crate::{tokens::{Identifier, LiteralInt}, NamedSyntaxNode, SyntaxNode};
 
-#[derive(Debug, Clone, PartialEq)]
-pub struct EnumDeclaration {
-    pub name: Spanned<Identifier>,
-    pub body: Spanned<EnumBody>
+
+#[derive(Debug, Clone)]
+pub struct EnumDeclaration;
+
+impl NamedSyntaxNode for EnumDeclaration {
+    const NODE_NAME: &'static str = "enum_decl_stmt";
 }
 
-#[derive(Debug, Clone, PartialEq)]
-pub struct EnumDeclarationValue {
-    pub name: Spanned<Identifier>,
-    pub int_value: Option<Spanned<i32>>
+impl SyntaxNode<'_, EnumDeclaration> {
+    pub fn name(&self) -> SyntaxNode<'_, Identifier> {
+        self.field_child("name").unwrap().into()
+    }
+
+    pub fn definition(&self) -> SyntaxNode<'_, EnumBlock> {
+        self.field_child("definition").unwrap().into()
+    }
 }
 
-pub type EnumBody = Vec<Spanned<EnumDeclarationValue>>;
+
+
+#[derive(Debug, Clone)]
+pub struct EnumBlock;
+
+impl NamedSyntaxNode for EnumBlock {
+    const NODE_NAME: &'static str = "enum_block";
+}
+
+impl SyntaxNode<'_, EnumBlock> {
+    pub fn values(&self) -> impl Iterator<Item = SyntaxNode<'_, EnumDeclarationValue>> {
+        self.children().map(|n| n.into())
+    }
+}
+
+
+
+#[derive(Debug, Clone)]
+pub struct EnumDeclarationValue;
+
+impl NamedSyntaxNode for EnumDeclarationValue {
+    const NODE_NAME: &'static str = "enum_decl_value";
+}
+
+impl SyntaxNode<'_, EnumDeclarationValue> {
+    pub fn name(&self) -> SyntaxNode<'_, Identifier> {
+        self.field_child("name").unwrap().into()
+    }
+
+    pub fn value(&self) -> SyntaxNode<'_, LiteralInt> {
+        self.field_child("value").unwrap().into()
+    }
+}
--- core/src/ast/structs.rs
+++ core/src/ast/structs.rs
@@ -1,31 +1,124 @@
-use crate::tokens::*;
-use super::{vars::*, expressions::Expression};
+use std::str::FromStr;
+
+use crate::{tokens::{Identifier, Keyword, LiteralString}, NamedSyntaxNode, SyntaxNode};
+use super::{vars::MemberVarDeclaration, expressions::Expression};
 
 
-#[derive(Debug, Clone, PartialEq)]
-pub struct StructDeclaration {
-    pub imported: bool,
-    pub name: Spanned<Identifier>,
-    pub body: Spanned<StructBody>
+#[derive(Debug, Clone)]
+pub struct StructDeclaration;
+
+impl NamedSyntaxNode for StructDeclaration {
+    const NODE_NAME: &'static str = "struct_decl_stmt";
 }
 
-#[derive(Debug, Clone, PartialEq)]
-pub enum StructStatement {
-    Var(MemberVarDeclaration),
-    Default(MemberDefaultValue),
-    Hint(MemberHint),
+impl SyntaxNode<'_, StructDeclaration> {
+    pub fn specifiers(&self) -> impl Iterator<Item = SyntaxNode<'_, StructSpecifier>> {
+        self.field_children("specifiers").map(|n| n.into())
+    }
+
+    pub fn name(&self) -> SyntaxNode<'_, Identifier> {
+        self.field_child("name").unwrap().into()
+    }
+
+    pub fn definition(&self) -> SyntaxNode<'_, StructBlock> {
+        self.field_child("definition").unwrap().into()
+    }
 }
 
-#[derive(Debug, Clone, PartialEq)]
-pub struct MemberDefaultValue {
-    pub member: Spanned<Identifier>,
-    pub value: Box<Spanned<Expression>>,
+
+
+#[derive(Debug, Clone)]
+pub struct StructBlock;
+
+impl NamedSyntaxNode for StructBlock {
+    const NODE_NAME: &'static str = "struct_block";
 }
 
-#[derive(Debug, Clone, PartialEq)]
-pub struct MemberHint {
-    pub member: Spanned<Identifier>,
-    pub value: Spanned<String>
+impl SyntaxNode<'_, StructBlock> {
+    pub fn statements(&self) -> impl Iterator<Item = SyntaxNode<'_, StructStatement>> {
+        self.children().map(|n| n.into())
+    }
 }
 
-pub type StructBody = Vec<Spanned<StructStatement>>;
+
+
+#[derive(Debug, Clone)]
+pub enum StructStatement<'script> {
+    Var(SyntaxNode<'script, MemberVarDeclaration>),
+    Default(SyntaxNode<'script, MemberDefaultValue>),
+    Hint(SyntaxNode<'script, MemberHint>),
+}
+
+impl SyntaxNode<'_, StructStatement<'_>> {
+    pub fn value(&self) -> StructStatement {
+        match self.tree_node.kind() {
+            MemberVarDeclaration::NODE_NAME => StructStatement::Var(self.clone().into()),
+            MemberDefaultValue::NODE_NAME => StructStatement::Default(self.clone().into()),
+            MemberHint::NODE_NAME => StructStatement::Hint(self.clone().into()),
+            _ => panic!("Unknown struct statement type: {}", self.tree_node.kind())
+        }
+    }
+}
+
+
+
+#[derive(Debug, Clone)]
+pub struct MemberDefaultValue; 
+
+impl NamedSyntaxNode for MemberDefaultValue {
+    const NODE_NAME: &'static str = "member_default_val_stmt";
+}
+
+impl SyntaxNode<'_, MemberDefaultValue> {
+    pub fn member(&self) -> SyntaxNode<'_, Identifier> {
+        self.field_child("member").unwrap().into()
+    }
+
+    pub fn value(&self) -> SyntaxNode<'_, Expression<'_>> {
+        self.field_child("value").unwrap().into()
+    }
+}
+
+
+
+#[derive(Debug, Clone)]
+pub struct MemberHint; 
+
+impl NamedSyntaxNode for MemberHint {
+    const NODE_NAME: &'static str = "member_hint_stmt";
+}
+
+impl SyntaxNode<'_, MemberHint> {
+    pub fn member(&self) -> SyntaxNode<'_, Identifier> {
+        self.field_child("member").unwrap().into()
+    }
+
+    pub fn value(&self) -> SyntaxNode<'_, LiteralString> {
+        self.field_child("value").unwrap().into()
+    }
+}
+
+
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+pub enum StructSpecifier {
+    Import
+}
+
+impl NamedSyntaxNode for StructSpecifier {
+    const NODE_NAME: &'static str = "struct_specifier";
+}
+
+impl SyntaxNode<'_, StructSpecifier> {
+    pub fn value(&self) -> StructSpecifier {
+        let s = self.text();
+        if let Ok(k) = Keyword::from_str(s.as_str()) {
+            match k {
+                Keyword::Import => return StructSpecifier::Import,
+                _ => {}
+            }
+        }
+
+        panic!("Unknown struct specifier: {}", s)
+    }
+}
