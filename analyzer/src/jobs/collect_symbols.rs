ae7caa3a2d00894c96422216e2e3027cdb354605
SpontanCombust <61706594+SpontanCombust@users.noreply.github.com>
2023-12-02 00:29:33 +0100

    diagnostic type instead of error

--- analyzer/src/jobs/collect_symbols.rs
+++ analyzer/src/jobs/collect_symbols.rs
@@ -1,27 +1,10 @@
 use ropey::Rope;
-use thiserror::Error;
 use uuid::Uuid;
 use witcherscript::tokens::Identifier;
 use witcherscript::{SyntaxNode, DocSpan};
 use witcherscript::ast::*;
+use crate::diagnostics::*;
 use crate::model::{collections::*, symbols::*};
-
-
-#[derive(Debug, Error)]
-pub enum SymbolCollectionError {
-    #[error("missing symbol at {}", .0.start)]
-    NodeMissing(DocSpan),
-    #[error("duplicate declaration of type {} at {}", symbol_name, .duplicate_appearance.start)]
-    DuplicateDefinition{
-        symbol_name: String,
-        // first_appearance: DocSpan, //TODO symbols storing their spans
-        duplicate_appearance: DocSpan 
-    },
-    #[error("type {} is missing", .0.start)]
-    TypeMissing(DocSpan),
-}
-
-use SymbolCollectionError::*;
 
 
 struct TypeCollectingVisitor {
@@ -29,39 +12,48 @@ struct TypeCollectingVisitor {
     symtab: SymbolTable,
     script_id: Uuid,
     rope: Rope,
-    errors: Vec<SymbolCollectionError>,
+    diagnostics: Vec<Diagnostic>,
 }
 
 impl TypeCollectingVisitor {
     /// If identifier node is missing appends an error and returns None. Otherwise returns identifier text.
-    fn check_missing_node(&mut self, ident_node: &SyntaxNode<'_, Identifier>) -> Option<String> {
+    fn check_missing_ident(&mut self, ident_node: &SyntaxNode<'_, Identifier>) -> Option<String> {
         if let Some(ident) = ident_node.value(&self.rope) { 
             Some(ident.into())
         } else {
-            self.errors.push(NodeMissing(ident_node.span()));
+            self.diagnostics.push(Diagnostic { 
+                span: ident_node.span(), 
+                severity: DiagnosticSeverity::Error, 
+                body: DiagnosticBody::MissingElement { what: "identifier".into() }
+            });
             None
         }
     }
 
     /// If symbol is a duplicate appends an error and returns false, otherwise returns true.
-    fn check_duplicate(&mut self, sym_name: &str, span: DocSpan) -> bool {
-        if self.symtab.contains_key(sym_name) {
-            self.errors.push(DuplicateDefinition { 
-                symbol_name: sym_name.to_owned(), 
-                duplicate_appearance: span
+    fn check_duplicate(&mut self, sym_name: &str, sym_type: SymbolType, span: DocSpan) -> bool {
+        if let Some(val) = self.symtab.get(sym_name) {
+            self.diagnostics.push(Diagnostic { 
+                span, 
+                severity: DiagnosticSeverity::Error, 
+                body: DiagnosticBody::SymbolNameTaken { 
+                    name: sym_name.to_string(), 
+                    this_type: sym_type, 
+                    precursor_type: val.typ 
+                }
             });
 
-            true
-        } else {
             false
+        } else {
+            true
         }
     }
 }
 
 impl StatementVisitor for TypeCollectingVisitor {
     fn visit_class_decl(&mut self, n: &SyntaxNode<'_, ClassDeclaration>) -> bool {
-        if let Some(c) = self.check_missing_node(&n.name()) {
-            if self.check_duplicate(c.as_str(), n.span()) {
+        if let Some(c) = self.check_missing_ident(&n.name()) {
+            if self.check_duplicate(c.as_str(), SymbolType::Class, n.span()) {
                 let sym = ClassSymbol::new(self.script_id, c.as_str());
                 self.symtab.insert(c, SymbolTableValue::from_symbol(&sym));
                 self.db.classes.insert(sym.symbol_id(), sym);
@@ -72,9 +64,9 @@ impl StatementVisitor for TypeCollectingVisitor {
     }
 
     fn visit_state_decl(&mut self, n: &SyntaxNode<'_, StateDeclaration>) -> bool {
-        if let (Some(state_name), Some(parent_name)) = (self.check_missing_node(&n.name()), self.check_missing_node(&n.parent())) {
+        if let (Some(state_name), Some(parent_name)) = (self.check_missing_ident(&n.name()), self.check_missing_ident(&n.parent())) {
             let state_class = StateSymbol::class_name(&state_name, &parent_name);
-            if self.check_duplicate(state_class.as_str(), n.span()) {
+            if self.check_duplicate(state_class.as_str(), SymbolType::State, n.span()) {
                 let sym = StateSymbol::new(self.script_id, state_class.as_str());
                 self.symtab.insert(state_class, SymbolTableValue::from_symbol(&sym));
                 self.db.states.insert(sym.symbol_id(), sym);
@@ -85,8 +77,8 @@ impl StatementVisitor for TypeCollectingVisitor {
     }
 
     fn visit_struct_decl(&mut self, n: &SyntaxNode<'_, StructDeclaration>) -> bool {
-        if let Some(s) = self.check_missing_node(&n.name()) {
-            if self.check_duplicate(s.as_str(), n.span()) {
+        if let Some(s) = self.check_missing_ident(&n.name()) {
+            if self.check_duplicate(s.as_str(), SymbolType::Struct, n.span()) {
                 let sym = StructSymbol::new(self.script_id, s.as_str());
                 self.symtab.insert(s, SymbolTableValue::from_symbol(&sym));
                 self.db.structs.insert(sym.symbol_id(), sym);
@@ -97,8 +89,8 @@ impl StatementVisitor for TypeCollectingVisitor {
     }
 
     fn visit_enum_decl(&mut self, n: &SyntaxNode<'_, EnumDeclaration>) -> bool {
-        if let Some(e) = self.check_missing_node(&n.name()) {
-            if self.check_duplicate(e.as_str(), n.span()) {
+        if let Some(e) = self.check_missing_ident(&n.name()) {
+            if self.check_duplicate(e.as_str(), SymbolType::Enum, n.span()) {
                 let sym = EnumSymbol::new(self.script_id, e.as_str());
                 self.symtab.insert(e, SymbolTableValue::from_symbol(&sym));
                 self.db.enums.insert(sym.symbol_id(), sym);
--- analyzer/src/lib.rs
+++ analyzer/src/lib.rs
@@ -1,2 +1,3 @@
 pub mod model;
 pub mod jobs;
+pub mod diagnostics;
