cfd9eefda384d723275b060bb596dcceae7b3d1a
SpontanCombust <61706594+SpontanCombust@users.noreply.github.com>
2024-01-19 17:27:00 +0100

    xtask: build before copying lsp

--- xtask/src/commands/copy_lsp.rs
+++ xtask/src/commands/copy_lsp.rs
@@ -1,4 +1,4 @@
-use xshell::Shell;
+use xshell::{Shell, cmd};
 
 
 const SRC: &str = "./target/debug/witcherscript-lsp.exe";
@@ -6,6 +6,9 @@ const DST: &str = "./editors/vscode/server/bin";
 
 pub fn copy_lsp() -> anyhow::Result<()> {
     let sh = Shell::new()?;
+
+    println!("Building the LSP...");
+    cmd!(sh, "cargo build --package witcherscript-lsp").run()?;
 
     sh.copy_file(SRC, DST)?;
     println!("Copied debug LSP into {}", DST);
--- xtask/src/commands/copy_lsp_release.rs
+++ xtask/src/commands/copy_lsp_release.rs
@@ -1,4 +1,4 @@
-use xshell::Shell;
+use xshell::{Shell, cmd};
 
 
 const SRC: &str = "./target/release/witcherscript-lsp.exe";
@@ -7,6 +7,9 @@ const DST: &str = "./editors/vscode/server/bin";
 pub fn copy_lsp_release() -> anyhow::Result<()> {
     let sh = Shell::new()?;
 
+    println!("Building the LSP...");
+    cmd!(sh, "cargo build --package witcherscript-lsp --release").run()?;
+    
     sh.copy_file(SRC, DST)?;
     println!("Copied release LSP into {}", DST);
 
