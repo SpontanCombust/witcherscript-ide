b2f6b8b8d434165f18f3f9391253270717808c37
SpontanCombust <61706594+SpontanCombust@users.noreply.github.com>
2024-02-02 03:45:54 +0100

    Extension configuration settings #6

---
+++ crates/lsp/src/config.rs
@@ -0,0 +1,41 @@
+use std::path::PathBuf;
+use thiserror::Error;
+use tower_lsp::jsonrpc;
+use tower_lsp::Client;
+use tower_lsp::lsp_types as lsp;
+
+
+#[derive(Debug, Clone)]
+pub struct Config {
+    pub game_directory: PathBuf,
+    pub project_search_directories: Vec<PathBuf>
+}
+
+#[derive(Debug, Error)]
+pub enum ConfigError {
+    #[error("Client returned an error")]
+    ClientResponseError(#[from] jsonrpc::Error),
+    #[error("Data coming from the client failed to deserialize")]
+    DeserializationError(#[from] serde_json::Error)
+}
+
+impl Config {
+    const CONFIG_ITEM_SECTIONS: [&str; 2] = [
+        "witcherscript-ide.gameDirectory",
+        "witcherscript-ide.projectSearchDirectories"
+    ];
+
+    pub async fn fetch(client: &Client) -> Result<Self, ConfigError> {
+        let config_items = Self::CONFIG_ITEM_SECTIONS.map(|section| lsp::ConfigurationItem {
+            scope_uri: None,
+            section: Some(section.to_string())
+        }).to_vec();
+
+        let values = client.configuration(config_items).await?;
+
+        Ok(Self {
+            game_directory: serde_json::from_value(values[0].clone())?,
+            project_search_directories: serde_json::from_value(values[1].clone())?
+        })
+    }
+}
--- crates/lsp/src/main.rs
+++ crates/lsp/src/main.rs
@@ -1,68 +1,97 @@
 use dashmap::DashMap;
+use tokio::sync::RwLock;
 use tower_lsp::jsonrpc::Result;
-use tower_lsp::lsp_types::*;
+use tower_lsp::lsp_types::notification::Notification;
+use tower_lsp::lsp_types as lsp;
 use tower_lsp::{Client, LanguageServer, LspService, Server};
 use witcherscript::Script;
 use witcherscript::script_document::ScriptDocument;
+use crate::config::Config;
 
 mod providers;
+mod config;
 
 
 #[derive(Debug)]
 pub struct Backend {
     client: Client,
-    doc_buffers: DashMap<Url, ScriptDocument>,
-    scripts: DashMap<Url, Script> // temporary solution, use types provided from witcherscript_project later
+    config: RwLock<Option<Config>>,
+    doc_buffers: DashMap<lsp::Url, ScriptDocument>,
+    scripts: DashMap<lsp::Url, Script>
 }
 
 impl Backend {
+    pub const LANGUAGE_ID: &str = "witcherscript";
+    pub const SERVER_NAME: &str = "witcherscript-ide";
+
     fn new(client: Client) -> Self {
         Self {
             client,
+            config: RwLock::new(None),
             doc_buffers: DashMap::new(),
-            scripts: DashMap::new() 
+            scripts: DashMap::new()
+        }
+    }
+
+    async fn fetch_config(&self) {
+        match Config::fetch(&self.client).await {
+            Ok(config) => {
+                let mut lock = self.config.write().await;
+                *lock = Some(config);
+            },
+            Err(err) => {
+                self.client.log_message(lsp::MessageType::ERROR, format!("Client configuration fetch error: {}", err)).await;
+            },
         }
     }
 }
 
 #[tower_lsp::async_trait]
 impl LanguageServer for Backend {
-    async fn initialize(&self, _: InitializeParams) -> Result<InitializeResult> {
-        Ok(InitializeResult {
-            server_info: Some(ServerInfo {
-                name: "witcherscript-ide".into(),
+    async fn initialize(&self, _: lsp::InitializeParams) -> Result<lsp::InitializeResult> {
+        Ok(lsp::InitializeResult {
+            server_info: Some(lsp::ServerInfo {
+                name: Backend::SERVER_NAME.into(),
                 version: Some(env!("CARGO_PKG_VERSION").into())
             }),
-            capabilities: ServerCapabilities {
-                text_document_sync: Some(TextDocumentSyncCapability::Options(TextDocumentSyncOptions {
+            capabilities: lsp::ServerCapabilities {
+                text_document_sync: Some(lsp::TextDocumentSyncCapability::Options(lsp::TextDocumentSyncOptions {
                     open_close: Some(true),
-                    change: Some(TextDocumentSyncKind::INCREMENTAL),
+                    change: Some(lsp::TextDocumentSyncKind::INCREMENTAL),
                     will_save: Some(false),
                     will_save_wait_until: Some(false),
-                    save: Some(TextDocumentSyncSaveOptions::SaveOptions(SaveOptions {
+                    save: Some(lsp::TextDocumentSyncSaveOptions::SaveOptions(lsp::SaveOptions {
                         include_text: Some(false)
                     }))
                 })),
-                ..ServerCapabilities::default()
+                ..lsp::ServerCapabilities::default()
             }
         })
     }
 
-    async fn initialized(&self, _: InitializedParams) {
-        self.client
-            .log_message(MessageType::INFO, "initialized!")
-            .await;
+    async fn initialized(&self, _: lsp::InitializedParams) {
+        self.client.log_message(lsp::MessageType::INFO, "Server initialized!").await;
+
+        self.client.register_capability(vec![
+            lsp::Registration { 
+                id: lsp::notification::DidChangeConfiguration::METHOD.to_string(), 
+                method: lsp::notification::DidChangeConfiguration::METHOD.to_string(), 
+                register_options: None 
+            }
+        ]).await.unwrap();
+
+        self.fetch_config().await;
     }
 
     async fn shutdown(&self) -> Result<()> {
         Ok(())
     }
 
-    async fn did_open(&self, params: DidOpenTextDocumentParams) {
+    async fn did_open(&self, params: lsp::DidOpenTextDocumentParams) {
         providers::document_sync::did_open(self, params).await;
     }
 
-    async fn did_change(&self, params: DidChangeTextDocumentParams) {
+    async fn did_change(&self, params: lsp::DidChangeTextDocumentParams) {
         providers::document_sync::did_change(self, params).await;
     }
 
@@ -71,8 +100,12 @@ impl LanguageServer for Backend {
     //     providers::document_sync::did_save(self, params).await;
     // }
 
-    async fn did_close(&self, params: DidCloseTextDocumentParams) {
+    async fn did_close(&self, params: lsp::DidCloseTextDocumentParams) {
         providers::document_sync::did_close(self, params).await;
+    }
+
+    async fn did_change_configuration(&self, _: lsp::DidChangeConfigurationParams) {
+        self.fetch_config().await;
     }
 }
 
