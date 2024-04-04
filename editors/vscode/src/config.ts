f7dac588542ed2b48c0078e5c7155a463e14df58
SpontanCombust <cedro_p@o2.pl>
2024-04-04 21:14:49 +0200

    Amend 3f2c676f01255cec2c9ab12267d6c60ddc6efb30

--- editors/vscode/src/config.ts
+++ editors/vscode/src/config.ts
@@ -3,13 +3,13 @@ import * as vscode from 'vscode';
 
 export interface Config {
     gameDirectory: string,
-    projectRepositories: string[]
+    contentRepositories: string[]
 }
 
 export function getConfiguration(): Config {
     const config = vscode.workspace.getConfiguration('witcherscript-ide');
     return {
         gameDirectory: config.get<string>('gameDirectory') ?? '',
-        projectRepositories: config.get<string[]>('projectRepositories') ?? []
+        contentRepositories: config.get<string[]>('contentRepositories') ?? []
     }
 }
