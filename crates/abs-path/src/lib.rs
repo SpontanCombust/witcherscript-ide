d2ded39637c961c7ea8f8982a1c765ce6d5a5a42
SpontanCombust <61706594+SpontanCombust@users.noreply.github.com>
2024-04-09 00:49:33 +0200

    refactor `abs-path` crate, add `joins` test

--- crates/abs-path/src/lib.rs
+++ crates/abs-path/src/lib.rs
@@ -7,7 +7,7 @@ use thiserror::Error;
 /// A type that guarantees to be an absolute file path.
 /// This gives a type-safe way to mark a given parameter as needing of an absolute path.
 /// It does not access the file system to create the path.
-/// Also corrects separators into proper one sfor the given OS. 
+/// Also corrects separators into proper ones for host OS. 
 /// 
 /// For Windows paths removes verbatim and UNC prefixes, so only local paths will work.
 /// Does not support symbolic links.
@@ -19,7 +19,7 @@ pub struct AbsPath {
 impl AbsPath {
     /// If the path is relative uses the `cwd` parameter to resolve it. 
     /// If `cwd` is not supplied uses the current working directory of the process.
-    pub fn resolve<P: AsRef<Path>>(path: P, cwd: Option<&Self>) -> Result<Self, ImpossiblePathError> {
+    pub fn resolve<P: AsRef<Path>>(path: P, cwd: Option<&Self>) -> Result<Self, Error> {
         let path = path.as_ref();
         
         // not necessairly normalized, because `is_absolute` only checks the beginning of the path
@@ -30,18 +30,12 @@ impl AbsPath {
             cwd.inner.join(path)
         };
 
-        let (prefix_disk, rest) = Self::split(&unnormalized);
+        let (root, stem) = Self::split(&unnormalized)?;
         
-        let mut abs_path = prefix_disk.map(|prefix| {
-            PathBuf::from(format!("{}:", prefix))
-        })
-        .unwrap_or_default();
-
-        abs_path.push(MAIN_SEPARATOR_STR);
-
-        if !rest.as_os_str().is_empty() {
-            let rest = Self::normalize_relative(rest)?;
-            abs_path.push(rest);
+        let mut abs_path = root;
+        if !stem.as_os_str().is_empty() {
+            let stem = Self::normalize_stem(stem)?;
+            abs_path.push(stem);
         }
 
         Ok(Self { inner: abs_path })
@@ -49,15 +43,17 @@ impl AbsPath {
 
     /// Creates a new AbsPath by joining to it a `path`.
     /// If `path` is absolute it returns it instead of the expected sum of paths.
-    pub fn join<P: AsRef<Path>>(&self, path: P) -> Result<Self, ImpossiblePathError> {
+    pub fn join<P: AsRef<Path>>(&self, path: P) -> Result<Self, Error> {
         let path = path.as_ref();
         if path.as_os_str().is_empty() {
             Ok(self.clone())
         } else if path.is_absolute() {
             Self::resolve(path, None)
         } else {
-            let path = Self::normalize_relative(path)?;
-            Ok(Self { inner: self.inner.join(path) })
+            let (root, stem) = Self::split(&self.inner)?;
+            let stem = Self::normalize_stem(&stem.join(path))?;
+
+            Ok(Self { inner: root.join(stem) })
         }
     }
 
@@ -66,35 +62,42 @@ impl AbsPath {
     }
 
 
-    /// Divides the given path into prefix disk (if exists) and the relative path after the root
+    /// Divides the given path into a root and stem (relative path after the root)
+    /// On Windows the root also includes the drive prefix
     /// `unnormalized` should be an absolute, possibly unnormalized path
-    fn split(unnormalized: &Path) -> (Option<char>, &Path) {
+    fn split(unnormalized: &Path) -> Result<(PathBuf, &Path), Error> {
         let mut components = unnormalized.components();
-        let mut prefix = None;
+        let mut root = PathBuf::new();
 
-        if let Component::Prefix(prefix_comp) = components.next().unwrap() {
-            match prefix_comp.kind() {
-                Prefix::Disk(disk) | Prefix::VerbatimDisk(disk) => {
-                    prefix = Some((disk as char).to_ascii_uppercase());
-                },
-                _ => {}
-            };
-
-            components.next().unwrap();
+        while let Some(comp) = components.next() {
+            match comp {
+                Component::Prefix(prefix_comp) => {
+                    match prefix_comp.kind() {
+                        Prefix::Disk(disk) | Prefix::VerbatimDisk(disk) => {
+                            root.push(&format!("{}:", (disk as char).to_ascii_uppercase()));
+                        },
+                        _ => return Err(Error::UnsupportedPrefix)
+                    };
+                }
+                Component::RootDir => {
+                    root.push(MAIN_SEPARATOR_STR);
+                    break;
+                }
+                _ => panic!("Path was not absolute")
+            }
         }
 
-        (prefix, components.as_path())
+        Ok((root, components.as_path()))
     }
 
-    /// Normalizes a path relative to the root path
-    fn normalize_relative(path: &Path) -> Result<PathBuf, ImpossiblePathError> {
+    fn normalize_stem(path: &Path) -> Result<PathBuf, Error> {
         let mut normalized = PathBuf::with_capacity(path.as_os_str().len());
 
         for component in path.components() {
             match component {
                 Component::ParentDir => { 
                     if !normalized.pop() {
-                        return Err(ImpossiblePathError);
+                        return Err(Error::ImpossiblePath);
                     }
                 },
                 Component::Normal(comp) => {
@@ -150,12 +153,17 @@ impl Into<PathBuf> for AbsPath {
 }
 
 
-/// Appears for paths like "/../../" which can never be possible, because they go outside of the root path
 #[derive(Debug, Error, Clone)]
-#[error("Path points to a resource outside of root directory")]
-pub struct ImpossiblePathError;
+pub enum Error {
+    // Appears for paths like "/../../" which can never be possible, because they go outside of the root path
+    #[error("Path points to a resource outside of root directory")]
+    ImpossiblePath,
+    // Windows only. 
+    #[error("Windows path non-disk prefixes are not supported")]
+    UnsupportedPrefix
+}
 
-impl Into<std::io::Error> for ImpossiblePathError {
+impl Into<std::io::Error> for Error {
     fn into(self) -> std::io::Error {
         std::io::Error::new(std::io::ErrorKind::NotFound, self)
     }
@@ -194,101 +202,128 @@ mod test {
     #[test]
     fn pass_absolute() {
         if cfg!(windows) {
-            let p = Path::new(r#"C:\foo\bar baz\alpha\"#);
+            let p = Path::new(r"C:\foo\bar baz\alpha\");
             let abs = AbsPath::resolve(p, None).unwrap();
-            assert_eq!(abs.inner, Path::new(r#"C:\foo\bar baz\alpha"#));
+            assert_eq!(abs.inner, Path::new(r"C:\foo\bar baz\alpha"));
         } else {
-            let p = Path::new(r#"/foo/bar baz/alpha/"#);
+            let p = Path::new(r"/foo/bar baz/alpha/");
             let abs = AbsPath::resolve(p, None).unwrap();
-            assert_eq!(abs.inner, Path::new(r#"/foo/bar baz/alpha"#));
+            assert_eq!(abs.inner, Path::new(r"/foo/bar baz/alpha"));
         }
     }
 
     #[test]
     fn pass_absolute_with_cwd() {
         if cfg!(windows) {
-            let cwd = AbsPath::resolve(r#"D:\"#, None).unwrap();
-            let p = Path::new(r#"C:\foo\bar baz\alpha\"#);
+            let cwd = AbsPath::resolve(r"D:\", None).unwrap();
+            let p = Path::new(r"C:\foo\bar baz\alpha\");
             let abs = AbsPath::resolve(p, Some(&cwd)).unwrap();
-            assert_eq!(abs.inner, Path::new(r#"C:\foo\bar baz\alpha"#));
+            assert_eq!(abs.inner, Path::new(r"C:\foo\bar baz\alpha"));
         } else {
-            let cwd = AbsPath::resolve(r#"/"#, None).unwrap();
-            let p = Path::new(r#"/foo/bar baz/alpha/"#);
+            let cwd = AbsPath::resolve(r"/", None).unwrap();
+            let p = Path::new(r"/foo/bar baz/alpha/");
             let abs = AbsPath::resolve(p, Some(&cwd)).unwrap();
-            assert_eq!(abs.inner, Path::new(r#"/foo/bar baz/alpha"#));
+            assert_eq!(abs.inner, Path::new(r"/foo/bar baz/alpha"));
         }
     }
 
     #[test]
     fn pass_relative() {
         if cfg!(windows) {
-            let p = Path::new(r#"bar baz\alpha\"#);
+            let p = Path::new(r"bar baz\alpha\");
             let abs = AbsPath::resolve(p, None).unwrap();
-            assert_eq!(abs.inner, current_dir().inner.join(r#"bar baz\alpha"#));
+            assert_eq!(abs.inner, current_dir().inner.join(r"bar baz\alpha"));
         } else {
-            let p = Path::new(r#"bar baz/alpha/"#);
+            let p = Path::new(r"bar baz/alpha/");
             let abs = AbsPath::resolve(p, None).unwrap();
-            assert_eq!(abs.inner, current_dir().inner.join(r#"bar baz/alpha"#));
+            assert_eq!(abs.inner, current_dir().inner.join(r"bar baz/alpha"));
         }
     }
 
     #[test]
     fn pass_relative_with_cwd() {
         if cfg!(windows) {
-            let cwd = AbsPath::resolve(r#"D:\beta"#, None).unwrap();
-            let p = Path::new(r#"bar baz\alpha\"#);
+            let cwd = AbsPath::resolve(r"D:\beta", None).unwrap();
+            let p = Path::new(r"bar baz\alpha\");
             let abs = AbsPath::resolve(p, Some(&cwd)).unwrap();
-            assert_eq!(abs.inner, Path::new(r#"D:\beta\bar baz\alpha"#));
+            assert_eq!(abs.inner, Path::new(r"D:\beta\bar baz\alpha"));
         } else {
-            let cwd = AbsPath::resolve(r#"/beta"#, None).unwrap();
-            let p = Path::new(r#"bar baz/alpha/"#);
+            let cwd = AbsPath::resolve(r"/beta", None).unwrap();
+            let p = Path::new(r"bar baz/alpha/");
             let abs = AbsPath::resolve(p, Some(&cwd)).unwrap();
-            assert_eq!(abs.inner, Path::new(r#"/beta/bar baz/alpha"#));
+            assert_eq!(abs.inner, Path::new(r"/beta/bar baz/alpha"));
         }
     }
 
     #[test]
     fn fix_slashes() {
         if cfg!(windows) {
-            let p = Path::new(r#"C:\Foo/Bar Baz/Echo/1234\charlie.exe"#);
+            let p = Path::new(r"C:\Foo/Bar Baz/Echo/1234\charlie.exe");
             let abs = AbsPath::resolve(p, None).unwrap();
-            assert_eq!(abs.inner, Path::new(r#"C:\Foo\Bar Baz\Echo\1234\charlie.exe"#));
+            assert_eq!(abs.inner, Path::new(r"C:\Foo\Bar Baz\Echo\1234\charlie.exe"));
         } else {
             // On unix a backslash in a path is treated as an escape character instead of a separator
             // and so it doesn't get corrected to a forward slash.
             // Unlike how forward slashes are converted to backslashes on Windows.
 
-            // let p = Path::new(r#"/foo/Bar Baz\Echo/1234\charlie"#);
+            // let p = Path::new(r"/foo/Bar Baz\Echo/1234\charlie");
             // let abs = AbsPath::resolve(p, None).unwrap();
-            // assert_eq!(abs.inner, Path::new(r#"/foo/Bar Baz/Echo/1234/charlie"#));
+            // assert_eq!(abs.inner, Path::new(r"/foo/Bar Baz/Echo/1234/charlie"));
         }
     }
 
     #[test]
     fn normalization() {
         if cfg!(windows) {
-            let cwd = AbsPath::resolve(r#"E:\omega\.."#, None).unwrap();
-            let p = Path::new(r#"alpha_beta\.\gamma\..\.zeta\."#);
+            let cwd = AbsPath::resolve(r"E:\omega\..", None).unwrap();
+            let p = Path::new(r"alpha_beta\.\gamma\..\.zeta\.");
             let abs = AbsPath::resolve(p, Some(&cwd)).unwrap();
-            assert_eq!(abs.inner, Path::new(r#"E:\alpha_beta\.zeta"#));
+            assert_eq!(abs.inner, Path::new(r"E:\alpha_beta\.zeta"));
 
-            assert!(AbsPath::resolve(r#"..\"#, Some(&cwd)).is_err());
+            assert!(AbsPath::resolve(r"..\", Some(&cwd)).is_err());
         } else {
-            let cwd = AbsPath::resolve(r#"/omega/.."#, None).unwrap();
-            let p = Path::new(r#"alpha_beta/./gamma/../.zeta/."#);
+            let cwd = AbsPath::resolve(r"/omega/..", None).unwrap();
+            let p = Path::new(r"alpha_beta/./gamma/../.zeta/.");
             let abs = AbsPath::resolve(p, Some(&cwd)).unwrap();
-            assert_eq!(abs.inner, Path::new(r#"/alpha_beta/.zeta"#));
+            assert_eq!(abs.inner, Path::new(r"/alpha_beta/.zeta"));
 
-            assert!(AbsPath::resolve(r#"../"#, Some(&cwd)).is_err());
+            assert!(AbsPath::resolve(r"../", Some(&cwd)).is_err());
+        }
+    }
+
+    #[test]
+    fn joins() {
+        if cfg!(windows) {
+            let p = AbsPath::resolve(r"C:\", None).unwrap();
+
+            let abs = p.join(r"foo\.\bar").unwrap();
+            assert_eq!(abs.inner, Path::new(r"C:\foo\bar"));
+
+            let abs = p.join(r"D:\baz").unwrap();
+            assert_eq!(abs.inner, Path::new(r"D:\baz"));
+
+            let abs = p.join(r"foo\..\..");
+            assert!(abs.is_err());
+        } else {
+            let p = AbsPath::resolve(r"/", None).unwrap();
+
+            let abs = p.join(r"foo/./bar").unwrap();
+            assert_eq!(abs.inner, Path::new(r"/foo/bar"));
+
+            let abs = p.join(r"/baz").unwrap();
+            assert_eq!(abs.inner, Path::new(r"/baz"));
+
+            let abs = p.join(r"foo/../..");
+            assert!(abs.is_err());
         }
     }
 
     #[test]
     fn windows_handle_prefixes() {
         if cfg!(windows) {
-            let p = Path::new(r#"\\?\c:\i\hate\windows\paths"#);
+            let p = Path::new(r"\\?\c:\i\hate\windows\paths");
             let abs = AbsPath::resolve(p, None).unwrap();
-            assert_eq!(abs.inner, Path::new(r#"C:\i\hate\windows\paths"#));
+            assert_eq!(abs.inner, Path::new(r"C:\i\hate\windows\paths"));
         }
     }
 }
