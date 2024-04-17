SpontanCombust/witcherscript-ide

commit 1f3fd3559bb008bd656e647155327bbb8b6ea883

fixes
- fix needless script and diagnostics updates in `document_ops`
- fix diagnostics not displaying when project manifest can't be parsed
- fix content not being properly detected, when it was at the workspace root

crates/lsp/src/providers/document_ops.rs
@@ -32,7 +32,8 @@ pub async fn did_open(backend: &Backend, params: lsp::DidOpenTextDocumentParams)
                 is_foreign: true
             });
 
-            backend.on_scripts_modified([doc_path], true).await;
+            backend.on_scripts_modified([doc_path.clone()], true).await;
+            backend.reporter.commit_diagnostics(&doc_path).await;
         }
     } else if doc_path.file_name().unwrap() == Manifest::FILE_NAME && belongs_to_workspace {
         //TODO remove this and let projects be loaded with did_save
@@ -49,15 +50,13 @@ pub async fn did_open(backend: &Backend, params: lsp::DidOpenTextDocumentParams)
             // try rebuilding the graph but only if it's not already being rebuilt
             if let Ok(mut content_graph) = backend.content_graph.try_write() {
                 backend.build_content_graph(&mut content_graph).await;
+                backend.reporter.commit_all_diagnostics().await;
             }
         }
     }
-
-    backend.reporter.commit_all_diagnostics().await;
 }
 
 pub async fn did_change(backend: &Backend, params: lsp::DidChangeTextDocumentParams) {
-    //FIXME not checked if it's a script
     let doc_path = AbsPath::try_from(params.text_document.uri.clone()).unwrap();
     if let Some(mut entry) = backend.scripts.get_mut(&doc_path) {
         let script_state = entry.value_mut();
@@ -71,10 +70,10 @@ pub async fn did_change(backend: &Backend, params: lsp::DidChangeTextDocumentPar
         }
 
         script_state.modified_timestamp = FileTime::now();
-    }
 
-    backend.on_scripts_modified([doc_path.clone()], true).await;
-    backend.reporter.commit_diagnostics(&doc_path).await;
+        backend.on_scripts_modified([doc_path.clone()], true).await;
+        backend.reporter.commit_diagnostics(&doc_path).await;
+    } 
 }
 
 // Not all circumstances can be easily handled or even detected.

crates/lsp/src/tasks/content_indexing_tasks.rs
@@ -152,11 +152,13 @@ impl Backend {
                     modified: vec![]
                 });
             }
-            //FIXME should be done only if those manifests are deleted or outside of workspace
-            if let Some(project) = removed_content.as_any().downcast_ref::<ProjectDirectory>() {
-                self.reporter.purge_diagnostics(project.manifest_path());
-            } else if let Some(redkit_proj) = removed_content.as_any().downcast_ref::<RedkitProjectDirectory>() {
-                self.reporter.purge_diagnostics(redkit_proj.manifest_path());
+            
+            if !removed_content_path.exists() || !removed_node.in_workspace {
+                if let Some(project) = removed_content.as_any().downcast_ref::<ProjectDirectory>() {
+                    self.reporter.purge_diagnostics(project.manifest_path());
+                } else if let Some(redkit_proj) = removed_content.as_any().downcast_ref::<RedkitProjectDirectory>() {
+                    self.reporter.purge_diagnostics(redkit_proj.manifest_path());
+                }
             }
         }
 

crates/project/src/content_scanner.rs
@@ -43,11 +43,18 @@ impl ContentScanner {
     pub fn scan(&self) -> (Vec<Box<dyn Content>>, Vec<ContentScanError>) {
         let mut contents = Vec::new();
         let mut errors = Vec::new();
-        //FIXME Error not handled!
-        if let Ok(content) = try_make_content(&self.scan_root) {
-            contents.push(content);
-        } else {
-            self.find_content_in_directory(&self.scan_root, &mut contents, &mut errors);
+        
+        match try_make_content(&self.scan_root) {
+            Ok(content) => {
+                contents.push(content)
+            },
+            Err(err) => {
+                if matches!(err, ContentScanError::NotContent) {
+                    self.find_content_in_directory(&self.scan_root, &mut contents, &mut errors);
+                } else {
+                    errors.push(err);
+                }
+            },
         }
 
         if self.only_projects {

commit 669e4e65add2fe71a1159408010b0703160d9290

fix deadlock caused by 1f3fd35

crates/lsp/src/providers/document_ops.rs
@@ -34,6 +34,7 @@ pub async fn did_open(backend: &Backend, params: lsp::DidOpenTextDocumentParams)
 
 pub async fn did_change(backend: &Backend, params: lsp::DidChangeTextDocumentParams) {
     let doc_path = AbsPath::try_from(params.text_document.uri.clone()).unwrap();
+    let mut should_notify = false;
     if let Some(mut entry) = backend.scripts.get_mut(&doc_path) {
         let script_state = entry.value_mut();
 
@@ -47,9 +48,13 @@ pub async fn did_change(backend: &Backend, params: lsp::DidChangeTextDocumentPar
 
         script_state.modified_timestamp = FileTime::now();
 
+        should_notify = true;
+    } 
+
+    if should_notify {
         backend.on_scripts_modified([doc_path.clone()], true).await;
         backend.reporter.commit_diagnostics(&doc_path).await;
-    } 
+    }
 }
 
 // Not all circumstances can be easily handled or even detected.