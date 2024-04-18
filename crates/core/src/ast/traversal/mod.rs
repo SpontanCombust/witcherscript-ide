f44594b29a068b9044fb1ba5886aa9022ec1b68b
SpontanCombust <61706594+SpontanCombust@users.noreply.github.com>
2024-04-19 00:30:00 +0200

    fix compilation errors

--- crates/core/src/ast/traversal/mod.rs
+++ crates/core/src/ast/traversal/mod.rs
@@ -1,10 +1,8 @@
 mod policies;
 mod visitor;
-mod visitor_decorators;
 
 pub use policies::*;
 pub use visitor::*;
-pub use visitor_decorators::*;
 
 
 /// Traverse an expression node using left-recursion.
