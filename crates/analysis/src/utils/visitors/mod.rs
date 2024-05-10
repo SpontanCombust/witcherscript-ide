c2a651d3df7ea28cfef4b27fad8e31be777eb5e2
SpontanCombust <61706594+SpontanCombust@users.noreply.github.com>
2024-05-10 17:48:45 +0200

    `SymbolPathBuilder` utility visitor

--- crates/analysis/src/utils/visitors/mod.rs
+++ crates/analysis/src/utils/visitors/mod.rs
@@ -1,2 +1,5 @@
 mod position_seeker;
 pub use position_seeker::{PositionSeeker, PositionSeekerPayload};
+
+mod sympath_builder;
+pub use sympath_builder::{SymbolPathBuilder, SymbolPathBuilderPayload};
