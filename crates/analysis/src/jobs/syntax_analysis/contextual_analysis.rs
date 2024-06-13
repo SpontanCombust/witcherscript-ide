e3a51893dbc36b6dba87109c9c6768b3eabf3593
SpontanCombust <cedro_p@o2.pl>
2024-06-13 22:32:38 +0200

    fix specifier checks for annotated functions

--- crates/analysis/src/jobs/syntax_analysis/contextual_analysis.rs
+++ crates/analysis/src/jobs/syntax_analysis/contextual_analysis.rs
@@ -22,7 +22,7 @@ struct ContextualSyntaxAnalysis<'a> {
 }
 
 impl ContextualSyntaxAnalysis<'_> {
-    fn visit_annotation(&mut self, n: &AnnotationNode, annotated_node_kind: &str, annotated_node_range: lsp::Range) {
+    fn visit_annotation(&mut self, n: &AnnotationNode, annotated_node_kind: &str, annotated_node_range: lsp::Range) -> Option<AnnotationKind> {
         let name_node = n.name();
         let name = name_node.value(self.doc);
         match AnnotationKind::from_str(&name) {
@@ -33,7 +33,7 @@ impl ContextualSyntaxAnalysis<'_> {
                         kind: DiagnosticKind::MissingAnnotationArgument { 
                             missing: kind.arg_type().unwrap_or_default().to_string()
                         }
-                    })
+                    });
                 }
 
                 match kind {
@@ -47,7 +47,7 @@ impl ContextualSyntaxAnalysis<'_> {
                                     annotation_name: name.to_string(),
                                     expected_text: "a method declaration".into()
                                 }
-                            })
+                            });
                         }
                     },
                     AnnotationKind::AddField => {
@@ -58,17 +58,97 @@ impl ContextualSyntaxAnalysis<'_> {
                                     annotation_name: name.to_string(),
                                     expected_text: "a field declaration".into()
                                 }
-                            })
+                            });
                         }
                     }
                 }
+
+                Some(kind)
             },
             Err(_) => {
                 self.diagnostics.push(Diagnostic {
                     range: name_node.range(),
                     kind: DiagnosticKind::InvalidAnnotation
-                })
+                });
+
+                None
             },
+        }
+    }
+
+    fn check_function_specifiers(&mut self, n: &FunctionDeclarationNode, global: bool) {
+        if global {
+            let mut specifiers = SmallVec::<[GlobalFunctionSpecifier; 2]>::new();
+            for (spec, range) in n.specifiers().map(|specn| (specn.value(), specn.range())) {
+                if let Ok(func_spec) = GlobalFunctionSpecifier::try_from(spec) {
+                    if !specifiers.contains(&func_spec) {
+                        specifiers.push(func_spec);
+                    } else {
+                        self.diagnostics.push(Diagnostic { 
+                            range, 
+                            kind: DiagnosticKind::RepeatedSpecifier
+                        });
+                    }
+                } else {
+                    let spec_name = Keyword::from(spec).to_string();
+                    self.diagnostics.push(Diagnostic {
+                        range,
+                        kind: DiagnosticKind::IncompatibleSpecifier { spec_name, sym_name: "a global function".into() }
+                    })
+                } 
+            }
+
+            if let Some((flavour, range)) = n.flavour().map(|f| (f.value(), f.range())) {
+                if GlobalFunctionFlavour::try_from(flavour).is_err() {
+                    let flavour_name = Keyword::from(flavour).to_string();
+                    self.diagnostics.push(Diagnostic {
+                        range,
+                        kind: DiagnosticKind::IncompatibleFunctionFlavour { flavour_name, sym_name: "a global function".into() }
+                    });
+                }
+            };
+        } else {
+            let mut specifiers = SmallVec::<[MemberFunctionSpecifier; 4]>::new();
+            let mut found_access_modif_before = false;
+
+            for (spec, range) in n.specifiers().map(|specn| (specn.value(), specn.range())) {
+                if let Ok(func_spec) = MemberFunctionSpecifier::try_from(spec) {
+                    if matches!(func_spec, MemberFunctionSpecifier::AccessModifier(_)) {
+                        if found_access_modif_before {
+                            self.diagnostics.push(Diagnostic { 
+                                range, 
+                                kind: DiagnosticKind::MultipleAccessModifiers
+                            });
+                        }
+                        found_access_modif_before = true;
+                    }
+
+                    if !specifiers.contains(&func_spec) {
+                        specifiers.push(func_spec);
+                    } else {
+                        self.diagnostics.push(Diagnostic { 
+                            range, 
+                            kind: DiagnosticKind::RepeatedSpecifier
+                        });
+                    }
+                } else {
+                    let spec_name = Keyword::from(spec).to_string();
+                    self.diagnostics.push(Diagnostic {
+                        range,
+                        kind: DiagnosticKind::IncompatibleSpecifier { spec_name, sym_name: "a method".into() }
+                    })
+                } 
+            }
+
+            if let Some((flavour, range)) = n.flavour().map(|f| (f.value(), f.range())) {
+                if MemberFunctionFlavour::try_from(flavour).is_err() {
+                    let flavour_name = Keyword::from(flavour).to_string();
+                    self.diagnostics.push(Diagnostic {
+                        range,
+                        kind: DiagnosticKind::IncompatibleFunctionFlavour { flavour_name, sym_name: "a method".into() }
+                    });
+                }
+            };
         }
     }
 }
@@ -160,39 +240,21 @@ impl SyntaxNodeVisitor for ContextualSyntaxAnalysis<'_> {
 
     fn visit_global_func_decl(&mut self, n: &FunctionDeclarationNode) -> FunctionDeclarationTraversalPolicy {
         if let Some(annot) = n.annotation() {
-            self.visit_annotation(&annot, FunctionDeclarationNode::NODE_KIND, n.range());
-        }
-
-        let mut specifiers = SmallVec::<[GlobalFunctionSpecifier; 2]>::new();
-
-        for (spec, range) in n.specifiers().map(|specn| (specn.value(), specn.range())) {
-            if let Ok(func_spec) = GlobalFunctionSpecifier::try_from(spec) {
-                if !specifiers.contains(&func_spec) {
-                    specifiers.push(func_spec);
-                } else {
-                    self.diagnostics.push(Diagnostic { 
-                        range, 
-                        kind: DiagnosticKind::RepeatedSpecifier
-                    });
+            if let Some(annot_kind) = self.visit_annotation(&annot, FunctionDeclarationNode::NODE_KIND, n.range()) {
+                let has_arg = annot.arg().is_some();
+                match (annot_kind, has_arg) {
+                    (AnnotationKind::AddMethod, _) => self.check_function_specifiers(n, false),
+                    (AnnotationKind::ReplaceMethod, has_arg) => self.check_function_specifiers(n, !has_arg),
+                    (AnnotationKind::WrapMethod, _) => self.check_function_specifiers(n, false),
+                    _ => self.check_function_specifiers(n, true)
                 }
-            } else {
-                let spec_name = Keyword::from(spec).to_string();
-                self.diagnostics.push(Diagnostic {
-                    range,
-                    kind: DiagnosticKind::IncompatibleSpecifier { spec_name, sym_name: "a global function".into() }
-                })
-            } 
-        }
-
-        if let Some((flavour, range)) = n.flavour().map(|f| (f.value(), f.range())) {
-            if GlobalFunctionFlavour::try_from(flavour).is_err() {
-                let flavour_name = Keyword::from(flavour).to_string();
-                self.diagnostics.push(Diagnostic {
-                    range,
-                    kind: DiagnosticKind::IncompatibleFunctionFlavour { flavour_name, sym_name: "a global function".into() }
-                });
             }
-        };
+            else {
+                self.check_function_specifiers(n, true);
+            }
+        } else {
+            self.check_function_specifiers(n, true);
+        }
 
         FunctionDeclarationTraversalPolicy { 
             traverse_params: true,
@@ -299,47 +361,7 @@ impl SyntaxNodeVisitor for ContextualSyntaxAnalysis<'_> {
             })
         }
 
-        let mut specifiers = SmallVec::<[MemberFunctionSpecifier; 4]>::new();
-        let mut found_access_modif_before = false;
-
-        for (spec, range) in n.specifiers().map(|specn| (specn.value(), specn.range())) {
-            if let Ok(func_spec) = MemberFunctionSpecifier::try_from(spec) {
-                if matches!(func_spec, MemberFunctionSpecifier::AccessModifier(_)) {
-                    if found_access_modif_before {
-                        self.diagnostics.push(Diagnostic { 
-                            range, 
-                            kind: DiagnosticKind::MultipleAccessModifiers
-                        });
-                    }
-                    found_access_modif_before = true;
-                }
-
-                if !specifiers.contains(&func_spec) {
-                    specifiers.push(func_spec);
-                } else {
-                    self.diagnostics.push(Diagnostic { 
-                        range, 
-                        kind: DiagnosticKind::RepeatedSpecifier
-                    });
-                }
-            } else {
-                let spec_name = Keyword::from(spec).to_string();
-                self.diagnostics.push(Diagnostic {
-                    range,
-                    kind: DiagnosticKind::IncompatibleSpecifier { spec_name, sym_name: "a method".into() }
-                })
-            } 
-        }
-
-        if let Some((flavour, range)) = n.flavour().map(|f| (f.value(), f.range())) {
-            if MemberFunctionFlavour::try_from(flavour).is_err() {
-                let flavour_name = Keyword::from(flavour).to_string();
-                self.diagnostics.push(Diagnostic {
-                    range,
-                    kind: DiagnosticKind::IncompatibleFunctionFlavour { flavour_name, sym_name: "a method".into() }
-                });
-            }
-        };
+        self.check_function_specifiers(n, false);
 
         FunctionDeclarationTraversalPolicy {
             traverse_params: true,
