7365e3ad17a7b19702de4ff6f500ebf77c8f38a4
SpontanCombust <61706594+SpontanCombust@users.noreply.github.com>
2024-07-22 17:05:45 +0200

    xtask: use built-in `curl` command instead of the `reqwest` library
    Removal of this dependency significantly cuts down on dependency footprint of xtask, since `reqwest` was using rather heavy libraries like `hyper`.

--- xtask/src/commands/prep_rw3d.rs
+++ xtask/src/commands/prep_rw3d.rs
@@ -1,6 +1,6 @@
 use std::path::MAIN_SEPARATOR_STR;
 use anyhow::Context;
-use xshell::Shell;
+use xshell::{cmd, Shell};
 
 
 #[cfg(target_os = "windows")]
@@ -15,16 +15,15 @@ pub fn prep_rw3d() -> anyhow::Result<()> {
     let root = project_root::get_project_root()?;
 
     let rw3d_zip_path = root.join("rw3d_cli.zip");
-    let mut rw3d_zip_file = std::fs::File::options()
-        .write(true)
-        .read(true)
-        .create(true)
-        .open(&rw3d_zip_path)
-        .context("Failed to create zip archive file")?;
+    let rw3d_zip_path_str = rw3d_zip_path.display().to_string();
 
     println!("Downloading rw3d_cli release zip file...");
-    let mut resp = reqwest::blocking::get(RW3D_RELEASE_URL).context("Failed to download rw3d release archive")?;
-    std::io::copy(&mut resp, &mut rw3d_zip_file).context("Failed to write downloaded artifact into a file")?;
+    cmd!(sh, "curl -L -o {rw3d_zip_path_str} {RW3D_RELEASE_URL}").run()?;
+
+    let rw3d_zip_file = std::fs::File::options()
+        .read(true)
+        .open(&rw3d_zip_path)
+        .context("Failed to read zip archive file")?;
     
     println!("Unzipping rw3d_cli into target destination...");
     let rw3d_bin_dst = root.join(RW3D_BIN_DST.replace("/", MAIN_SEPARATOR_STR));
