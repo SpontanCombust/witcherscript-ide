233942f6053004b4fb29cfcda6ae5aec55a5c2ed
SpontanCombust <61706594+SpontanCombust@users.noreply.github.com>
2024-08-02 21:26:10 +0200

    bumped version of rw3d_cli

--- xtask/src/commands/prep_rw3d.rs
+++ xtask/src/commands/prep_rw3d.rs
@@ -3,22 +3,33 @@ use anyhow::Context;
 use xshell::{cmd, Shell};
 
 
-#[cfg(target_os = "windows")]
-const RW3D_RELEASE_URL: &str = "https://github.com/SpontanCombust/rusty_witcher_debugger/releases/download/v0.6.1/rw3d_cli-v0.6.1-x86_64-pc-windows-msvc.zip";
-#[cfg(target_os = "linux")]
-const RW3D_RELEASE_URL: &str = "https://github.com/SpontanCombust/rusty_witcher_debugger/releases/download/v0.6.1/rw3d_cli-v0.6.1-x86_64-unknown-linux-gnu.zip";
-
+const RW3D_VERSION_TAG: &str = "v0.7.0";
 const RW3D_BIN_DST: &str = "editors/vscode/deps/rw3d/bin";
+
+
+fn rw3d_release_url() -> String {
+    let mut url = format!("https://github.com/SpontanCombust/rusty_witcher_debugger/releases/download/{RW3D_VERSION_TAG}/rw3d_cli-{RW3D_VERSION_TAG}-");
+
+    if cfg!(target_os = "windows") {
+        url += "x86_64-pc-windows-msvc";
+    } else {
+        url += "x86_64-unknown-linux-gnu";
+    }
+
+    url += ".zip";
+    url
+}
 
 pub fn prep_rw3d() -> anyhow::Result<()> {
     let sh = Shell::new()?;
     let root = project_root::get_project_root()?;
 
+    let rw3d_url = rw3d_release_url();
     let rw3d_zip_path = root.join("rw3d_cli.zip");
     let rw3d_zip_path_str = rw3d_zip_path.display().to_string();
 
     println!("Downloading rw3d_cli release zip file...");
-    cmd!(sh, "curl -L -o {rw3d_zip_path_str} {RW3D_RELEASE_URL}").run()?;
+    cmd!(sh, "curl -L -o {rw3d_zip_path_str} {rw3d_url}").run()?;
 
     let rw3d_zip_file = std::fs::File::options()
         .read(true)
